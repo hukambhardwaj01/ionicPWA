f39e17bc8b502b3d1958fa53859068e34691beb8
hukambhardwaj01 <hukambhardwaj01@ballistclearning.com>
2025-08-19 15:34:17 +0530

    Initial commit.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Geolocation, Position } from '@capacitor/geolocation';
+import { Share } from '@capacitor/share';
+import { IonHeader, IonImg } from "@ionic/angular/standalone";
+import { IonicModule } from "@ionic/angular";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
+  imports: [IonicModule,CommonModule],
 })
 export class HomePage {
 
+  myImage:any;
+  position: Position | any;
+
   constructor() {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+
+    this.myImage = image.webPath;
+  }
+
+    async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    this.position = coordinates;
+  }
+
+  async share() {
+    await Share.share({
+      title: 'Come and find me',
+      text: `Here's my current location: 
+        ${this.position.coords.latitude}, 
+        ${this.position.coords.longitude}`,
+      url: 'http://ionicacademy.com/'
+    });
+  }
+
 
 }
